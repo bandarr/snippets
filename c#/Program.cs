011efe26e772f2f9101d07f5d3ae735160d1a263
Dan Barr <dan.barr@outlook.com>
2024-01-14 20:52:56 -0500

    I did a multithread

---
+++ c#/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+
+class Program
+{
+  static void Main()
+  {
+    Thread t = new Thread(Worker);
+    t.Start();
+
+    for (int i = 0; i < 10; i++)
+    {
+      Console.WriteLine("Main thread doing some work");
+      Thread.Sleep(100);
+    }
+
+    t.Join();
+
+    Console.WriteLine("Done");
+  }
+
+  static void Worker()
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      Console.WriteLine("Worker thread doing some work");
+      Thread.Sleep(100);
+    }
+  }
+}
