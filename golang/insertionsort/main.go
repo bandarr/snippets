411cfac9b70ee5c9c1ca72d79425d7d72175f4e3
Dan Barr <dan.barr@outlook.com>
2024-02-02 21:56:33 -0500

    make more readable for learnding

---
+++ golang/insertionsort/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func insertionSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := i; j > 0; j-- {
+			if arr[j-1] > arr[j] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+			}
+		}
+	}
+	return arr
+}
+
+func main() {
+	fmt.Println(insertionSort([]int{5, 3, 2, 1, 0, 4}))
+}
